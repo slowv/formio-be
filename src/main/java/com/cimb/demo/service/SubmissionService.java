c00eb7d40705e257244986626e0b26ea5f70f660
smartosc.huynq <huynq7@smartosc.com>
2024-01-31 16:03:43 +0700

    [task] view submission by form

--- src/main/java/com/cimb/demo/service/SubmissionService.java
+++ src/main/java/com/cimb/demo/service/SubmissionService.java
@@ -1,7 +1,11 @@
 package com.cimb.demo.service;
 
 import com.cimb.demo.service.dto.SubmissionDTO;
+import com.cimb.demo.service.dto.request.PagingRequest;
+import com.cimb.demo.service.dto.response.PagingResponse;
 
 public interface SubmissionService {
     SubmissionDTO save(SubmissionDTO submissionDTO);
+
+    PagingResponse<SubmissionDTO> getSubmissionByForm(String formId, PagingRequest request);
 }
--- src/main/java/com/cimb/demo/service/impl/SubmissionServiceImpl.java
+++ src/main/java/com/cimb/demo/service/impl/SubmissionServiceImpl.java
@@ -2,15 +2,21 @@ package com.cimb.demo.service.impl;
 
 import com.cimb.demo.component.ValidatorDataExtractor;
 import com.cimb.demo.controller.errors.FormValidationException;
+import com.cimb.demo.entity.SubmissionEntity;
 import com.cimb.demo.repository.SubmissionRepository;
 import com.cimb.demo.service.SubmissionService;
 import com.cimb.demo.service.dto.SubmissionDTO;
+import com.cimb.demo.service.dto.request.PagingRequest;
+import com.cimb.demo.service.dto.response.PagingResponse;
 import com.cimb.demo.service.mapper.SubmissionMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -31,5 +37,16 @@ public class SubmissionServiceImpl implements SubmissionService {
         }
 
         throw new FormValidationException(HttpStatus.BAD_REQUEST.getReasonPhrase(), "validation", extract);
+    }
+
+    @Override
+    public PagingResponse<SubmissionDTO> getSubmissionByForm(String formId, PagingRequest request) {
+        log.debug("Request to get all submission by formId: {}", formId);
+        Page<SubmissionEntity> submissions = submissionRepository.findAllByFormId(formId, request.pageable());
+        List<SubmissionDTO> responses = submissions.stream().map(submissionMapper::toDto).toList();
+        return new PagingResponse<>(responses,
+                submissions.getTotalElements(),
+                submissions.getTotalPages(),
+                request);
     }
 }
